ee558c25786ddfc6540aef17933575e795a5999d
Brit <Brit@DESKTOP-BFORP22>
2022-06-24 16:44:57 -0700

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,11 @@
+﻿// class = A bundle of related code.
+//         Can be used as a blueprint to create objects (OOP)
+
+Messages.Hello();
+Messages.Waiting();
+Messages.Bye();
+
+
+
+Console.ReadKey();
+
